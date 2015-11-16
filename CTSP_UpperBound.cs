ae2e0360f6f52ab27c1e97105ea4439ffe5779a9
Jorge Blanchard <alu0100536591@edu.alu.es>
2015-11-16 20:06:32 +0000

    Implementado 2-opt y parece que funciona correctamente

--- CTSP_UpperBound.cs
+++ CTSP_UpperBound.cs
@@ -32,6 +32,8 @@ namespace Traveling_Salesman_Problem {
             _upperBound = 0;
 
             exec_NearestNeighbour();
+
+            _2opt();
         }
 
         public void exec_NearestNeighbour () {
@@ -52,7 +54,6 @@ namespace Traveling_Salesman_Problem {
                 _route.Add(currentVertex);
             }
         }
-
 
         private int find_nearbyUnvisitedVertex (int currentVertex) {
 
@@ -81,6 +82,69 @@ namespace Traveling_Salesman_Problem {
                     return true;
 
             return false;
+        }
+
+        private void _2opt () {
+
+            bool impovement = true;
+            decimal best_distance = 0;
+            while (impovement) {
+
+                impovement = false;
+
+                best_distance = calculateTotalDistance(_route);
+
+                impovement = _2opt_interation(best_distance);
+            }
+
+            _upperBound = best_distance;
+        }
+
+        private decimal calculateTotalDistance(List<int> route) {
+            decimal distance = 0;
+
+            for (int i = 1; i < route.Count; i++)
+                distance += _distances.Matrix[route[i - 1], route[i]];
+
+            return distance;
+        }
+
+        private bool _2opt_interation (decimal best_distance) {
+            for (int i = 0; i < _route.Count - 1; i++) {
+                for (int k = i + 1; k < _route.Count; k++) {
+
+                    List<int> new_route = _2optSwap(_route, i, k);
+
+                    decimal new_distance = calculateTotalDistance(new_route);
+
+                    if (new_distance < best_distance) {
+                        _route = new_route;
+                        return true;
+                    }
+
+                }
+            }
+
+            return false;
+        }
+
+        private List<int> _2optSwap (List<int> existing_route, int i, int k) {
+            List<int> new_route = new List<int>();
+
+            //1.take route[1] to route[i - 1] and add them in order to new_route
+            for (int n = 0; n < i; n++) 
+                new_route.Add(existing_route[n]);
+
+            //2.take route[i] to route[k] and add them in reverse order to new_route
+            for (int n = k; n >= i; n--)
+                new_route.Add(existing_route[n]);
+
+            //3. take route[k+1] to end and add them in order to new_route
+            for (int n = k+1; n < existing_route.Count; n++)
+                new_route.Add(existing_route[n]);
+
+            return new_route;
+
         }
 
 
